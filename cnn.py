3d0647b483a1a1e06eb2294bbba0baf40c744e7f
Rishabh Ranjan <mr.rishabh.ranjan@gmail.com>
2021-01-12 19:56:25 +0530

    cnn model job scripts

---
+++ cnn.py
@@ -0,0 +1,130 @@
+from imblearn.over_sampling import RandomOverSampler
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.utils.data as data
+
+class FaceDataset(data.Dataset):
+    def __init__(self, path, over_sample=False):
+        y, X = np.split(np.genfromtxt(path, delimiter=','), [1], axis=-1)
+        y = y.squeeze()
+        if over_sample:
+            X, y = RandomOverSampler().fit_resample(X, y)
+        X = X.reshape(-1, 1, 48, 48)
+        self.X = torch.FloatTensor(X)
+        self.y = torch.LongTensor(y)
+    
+    def __len__(self):
+        return self.y.shape[0]
+    
+    def __getitem__(self, i):
+        return self.X[i], self.y[i]
+
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(1, 64, 1, padding=0),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout(0.25),
+
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.Dropout(0.25),
+
+            nn.Conv2d(128, 256, 5, padding=2),
+            nn.ReLU(),
+            nn.BatchNorm2d(256),
+            nn.MaxPool2d(2),
+            nn.Dropout(0.25),
+
+            nn.Flatten(),
+
+            nn.Linear(24*24*256, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Dropout(0.25),
+
+            nn.Linear(128, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Dropout(0.25),
+
+            nn.Linear(256, 7)
+        )
+
+        self.criterion = nn.CrossEntropyLoss()
+
+    def forward(self, X):
+        return self.model(X)
+
+    def predict(self, X):
+        return torch.argmax(self.forward(X), dim=-1)
+
+def train(model, opt, train_loader, n_epochs, test_loader=None):
+    losses = []
+    for epoch in range(n_epochs):
+        model.train()
+        for X, y in train_loader:
+            X = X.to(device)
+            y = y.to(device)
+            yhat = model(X)
+            loss = model.criterion(yhat, y)
+            opt.zero_grad()
+            loss.backward()
+            nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+            opt.step()
+            losses.append(loss.item())
+        
+        torch.save(model.state_dict(), f'cnn_{lr}_{batch_size}_{epoch+1}.pt')
+        
+        if test_loader is not None:
+            print(f'#{epoch+1}:- ', end='')
+            model.eval()
+            true = []
+            pred = []
+            for X, y in test_loader:
+                X = X.to(device)
+                y = y.to(device)
+                yhat = model.predict(X)
+                true.extend(y.cpu().tolist())
+                pred.extend(yhat.cpu().tolist())
+            print(f'acc: {accuracy_score(true, pred):.5f}', end=' ')
+            print(f'p: {precision_score(true, pred, average="micro"):.5f}', end=' ')
+            print(f'r: {recall_score(true, pred, average="micro"):.5f}', end=' ')
+            print(f'f1: {f1_score(true, pred, average="micro"):.5f}', end=' ')
+            print(f'P: {precision_score(true, pred, average="macro"):.5f}', end=' ')
+            print(f'R: {recall_score(true, pred, average="macro"):.5f}', end=' ')
+            print(f'F1: {f1_score(true, pred, average="macro"):.5f}', flush=True)
+
+    return losses
+
+if __name__ == '__main__':
+    import sys
+    lr = float(sys.argv[1])
+    batch_size = int(sys.argv[2])
+    n_epochs = int(sys.argv[3])
+    print('lr:', lr)
+    print('batch_size:', batch_size)
+    print('n_epochs:', n_epochs)
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    print('device:', device)
+    print('loading data')
+    train_set = FaceDataset('train.csv', over_sample=True)
+    test_set = FaceDataset('public_test.csv')
+    print('train size:', len(train_set))
+    print('test size:', len(test_set))
+    model = Model().to(device)
+    opt = optim.Adam(model.parameters(), lr=lr)
+    train_loader = data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=4)
+    test_loader = data.DataLoader(test_set, batch_size=batch_size, shuffle=True, num_workers=4)
+    train_losses = train(model, opt, train_loader, n_epochs, test_loader)
+    plt.plot(train_losses)
+    plt.savefig('cnn.png')
+    print('training losses plotted in cnn.png')
---
+++ cnn.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+### Set the job name (for your reference)
+#PBS -N cnn_3_64 -e cnn_0.001_64.err -o cnn_0.001_64.out
+### Set the project name, your department code by default
+#PBS -P neurosub
+### Request email when job begins and ends
+#PBS -m bea
+### Specify email address to use for notification.
+#PBS -M $USER@iitd.ac.in
+####
+#PBS -l select=1:ncpus=6:ngpus=1:centos=skylake
+### Specify "wallclock time" required for this job, hhh:mm:ss
+#PBS -l walltime=2:00:00
+
+#PBS -l software=replace_with_Your_software_name
+# After job starts, must goto working directory. 
+# $PBS_O_WORKDIR is the directory from where the job is fired. 
+echo "==============================="
+echo $PBS_JOBID
+cat $PBS_NODEFILE
+echo "==============================="
+cd $PBS_O_WORKDIR
+#job 
+source ~/.bashrc
+conda activate col774
+python3 cnn.py 0.001 64 100
+#NOTE
+# The job line is an example : users need to change it to suit their applications
+# The PBS select statement picks n nodes each having m free processors
+# OpenMPI needs more options such as $PBS_NODEFILE
